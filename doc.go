d8c990e767fec4d0ee69cf1d24aa7f70a161e70f
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2022-11-11 19:15:01 +0200

    docs: Update doc.go

--- doc.go
+++ doc.go
@@ -1,3 +1,16 @@
 // Package dpo provides functionality for interacting with DPO Group's payment gateway from Go applications.
 // Currently the module only supports performing payments through DPOs verify token workflow.
+//
+// # Usage: User Agent
+//
+// You are recommended to set the user agent for the client to some string that identifies your application.
+//
+//		clientToken := os.Getenv("DPO_TOKEN")
+//		client := dpo.NewClient(clientToken, true)
+//		client.SetUserAgent("Example User Agent")
+//
+// # Usage: Error Handling
+//
+// The dpo package exposes errors that are thrown from DPO API.
+//
 package dpo
